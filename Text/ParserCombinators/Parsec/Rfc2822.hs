52a64dd666b09470f41a3208765207bb3b7fdf48
Peter Simons <simons@cryp.to>
2012-06-08 14:59:34 +0200

    Updated date in (c) lines, bumped version number, and updated tested-with fields.

--- Text/ParserCombinators/Parsec/Rfc2822.hs
+++ Text/ParserCombinators/Parsec/Rfc2822.hs
@@ -1,6 +1,6 @@
 {- |
    Module      :  Text.ParserCombinators.Parsec.Rfc2822
-   Copyright   :  (c) 2008 Peter Simons
+   Copyright   :  (c) 2012 Peter Simons
    License     :  BSD3
 
    Maintainer  :  simons@cryp.to
