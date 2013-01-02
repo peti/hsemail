7a99bab7346bdfb419eed0813d3ddd360f6480a2
Peter Simons <simons@cryp.to>
2013-01-02 13:08:07 +0100

    self-test.hs: improve structure of the test report

--- self-test.hs
+++ self-test.hs
@@ -23,27 +23,24 @@ main = hspec $ do
       parseTest date_time "Fri, 21 Dec 2012 00:07:43 +0300" `shouldReturn`
         CalendarTime 2012 December 21 0 7 43 0 Friday 0 "" 10800 False
 
-  describe "Rfc822.day" $
+  describe "Rfc822.day" $ do
     it "parses a hand-picked day-of-months correctly" $ do
       parseTest day "00" `shouldReturn` 0
       parseTest day "09" `shouldReturn` 9
       parseTest day "15" `shouldReturn` 15
 
-  describe "Rfc822.day" $
     it "does not perform range checking" $
       parseTest day "99" `shouldReturn` 99
 
-  describe "Rfc822.day" $
     it "fails properly on incomplete input" $ do
       parseFailure day "Mon"
       parseFailure day "Thu"
 
-  describe "Rfc822.obs_mbox_list" $
+  describe "Rfc822.obs_mbox_list" $ do
     it "parses hand-picked inputs correctly" $ do
       parseTest obs_mbox_list "," `shouldReturn` []
       parseTest obs_mbox_list "Joe Doe <joe@example.org>,( \r\n bla),,jane@\r\n example.net \r\n (Jane Doe)," `shouldReturn`
         [NameAddr (Just "Joe Doe") "joe@example.org",NameAddr Nothing "jane@example.net"]
 
-  describe "Rfc822.obs_mbox_list" $
     it "fails properly on incomplete input" $
       parseFailure obs_mbox_list "foo@example.org"
