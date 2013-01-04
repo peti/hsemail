d6881808bc90e4e3bcce83f5734eff9cbf53d839
Peter Simons <simons@cryp.to>
2013-01-04 13:13:46 +0100

    self-test.hs: add haddock header

--- self-test.hs
+++ self-test.hs
@@ -1,3 +1,15 @@
+{-
+   Module      :  Main
+   Copyright   :  (c) 2013 Peter Simons
+   License     :  BSD3
+
+   Maintainer  :  simons@cryp.to
+   Stability   :  provisional
+   Portability :  portable
+
+   HsEmail regression test suite.
+-}
+
 module Main ( main ) where
 
 import Test.Hspec
