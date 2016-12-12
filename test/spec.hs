9e0695ba22703e9532e880568299760d2c5bda32
Peter Simons <simons@cryp.to>
2016-12-12 09:26:41 +0100

    Clean up build system and directory structure.

--- test/spec.hs
+++ test/spec.hs
@@ -1,15 +1,3 @@
-{-
-   Module      :  Main
-   Copyright   :  (c) 2013 Peter Simons
-   License     :  BSD3
-
-   Maintainer  :  simons@cryp.to
-   Stability   :  provisional
-   Portability :  portable
-
-   HsEmail regression test suite.
--}
-
 module Main ( main ) where
 
 import Test.Hspec
